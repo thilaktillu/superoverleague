f03cdc10cf61959bbac14e0a34be1c578492b743
thilaktillu <malothuthilak@gmail.com>
2024-02-23 16:35:31 +0000

    super over league

---
+++ index.js
@@ -0,0 +1,9 @@
+const element = (
+  <div>
+    <h1>Super Over League</h1>
+    <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" />
+    <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" />
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
